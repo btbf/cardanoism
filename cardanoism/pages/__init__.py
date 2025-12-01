e91a23bc619251ddec134271f1240ecec2c1d1d7
BTBF <b.t.b.f.corp@gmail.com>
2025-12-02 02:54:16 +0400

    Remove settings page

--- cardanoism/pages/__init__.py
+++ cardanoism/pages/__init__.py
@@ -1,7 +1,6 @@
 from .catalyst import catalyst
 from .fund import fund
 from .index import index
-from .settings import settings
 from .contact import contact
 from .test import foreach_example
 #from .catalyst import dbtest, proposal_detail_page
