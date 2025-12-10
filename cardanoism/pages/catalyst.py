b8aa1a895da82dc7b76c751f86b7fae27745101b
BTBF <b.t.b.f.corp@gmail.com>
2025-12-11 02:29:32 +0400

    proposal page auto filter

--- cardanoism/pages/catalyst.py
+++ cardanoism/pages/catalyst.py
@@ -2,11 +2,11 @@ import reflex as rx
 from typing import Dict, List
 
 from cardanoism.templates import template
+from cardanoism.backend.db_connect import AppState, ProposalAppState, get_fund_options
 from cardanoism.components.proposal_card import card_foreach_dict
 from cardanoism.components.proposal_detail import detail_foreach_dict
 from cardanoism.components.proposal_pagenation import pagination_component
 from cardanoism.components.componets import top_button_component
-from cardanoism.backend.db_connect import AppState, ProposalAppState
 
 
 class ReactSelectLib(rx.Component):
@@ -16,40 +16,19 @@ class ReactSelectLib(rx.Component):
 
 class CatalystChallengeSelect(ReactSelectLib):
     is_default = True
-    isClearable = True
+    classNamePrefix: rx.Var[str]
+    isClearable: rx.Var[bool] = True
     isMulti: rx.Var[bool]
     placeholder: rx.Var[str]
     options: rx.Var[List[Dict[str, str]]]
+    controlShouldRenderValue: rx.Var[bool] = True
     defaultValue: rx.Var[Dict[str, str]]
     onChange: rx.EventHandler[lambda value: [value]]
 
 
 challegeFilter = CatalystChallengeSelect.create
 
-FUND_SELECT_OPTIONS = [
-    {"value": "147", "label": "Fund 14"},
-    {"value": "146", "label": "Fund 13"},
-    {"value": "139", "label": "Fund 12"},
-]
-
-CHALLENGE_OPTIONS = [
-    {"value": "153", "label": "F14 カルダノユースケース パートナー&製品"},
-    {"value": "154", "label": "F14 カルダノユースケース コンセプト"},
-    {"value": "155", "label": "F14 カルダノオープン エコシステム"},
-    {"value": "156", "label": "F14 カルダノオープン 開発者"},
-    {"value": "146", "label": "F13 カルダノオープン 開発者"},
-    {"value": "147", "label": "F13 カルダノオープン エコシステム"},
-    {"value": "149", "label": "F13 カルダノユースケース コンセプト"},
-    {"value": "150", "label": "F13 カルダノユースケース 製品"},
-    {"value": "151", "label": "F13 カルダノパートナー 企業"},
-    {"value": "152", "label": "F13 カルダノパートナー 成長"},
-    {"value": "142", "label": "F12 パートナーと実世界の統合"},
-    {"value": "143", "label": "F12 使用事例 コンセプト"},
-    {"value": "144", "label": "F12 使用事例 MVP"},
-    {"value": "145", "label": "F12 使用事例 製品"},
-    {"value": "140", "label": "F12 カルダノオープン 開発者"},
-    {"value": "141", "label": "F12 カルダノオープン エコシステム"},
-]
+FUND_SELECT_OPTIONS = get_fund_options()
 
 FUNDING_STATUS_OPTIONS = [
     {"value": "funded", "label": "採択"},
@@ -63,53 +42,105 @@ PROJECT_STATUS_OPTIONS = [
     {"value": "complete", "label": "完了"},
 ]
 
+FUND_SELECT_DARK_CSS = """
+<style>
+@media (prefers-color-scheme: dark) {
+  .filter__control {
+    background-color: var(--color-surface) !important;
+    border-color: var(--gray-a7) !important;
+    border-width: 1.4px !important;
+  }
+  .filter__menu {
+    background-color: var(--gray-3) !important;
+  }
+  .filter__option--is-focused {
+    background-color: var(--accent-10) !important;
+  }
+  .filter__placeholder {
+    color: var(--gray-10) !important;
+    font-weight: 450 !important;
+  }
+  .filter__multi-value {
+    background-color: var(--accent-9) !important;
+    }
+  .filter__multi-value__label {
+    color: var(--glay-4) !important;
+    }
+   .filter__multi-value__label:hover {
+    color: var(--gray-a12) !important;
+    background-color: var(--accent-10) !important;
+}
+</style>
+"""
 
-@template(route="/catalyst/", title="カタリストファンド", on_load=AppState.on_load)
+
+@template(route="/catalyst/", title="カタリスト", on_load=AppState.on_load)
 def catalyst() -> rx.Component:
     filters = rx.vstack(
+        rx.html(FUND_SELECT_DARK_CSS),
         rx.box(
             rx.input(
-                placeholder="全文検索...(IdeascaleNo、タイトル、課題、解決策など)",
+                placeholder="キーワード検索...(Ideascale番号・タイトルなど)",
                 size="3",
                 max_length=100,
-                on_change=lambda value: AppState.set_inputed_value(value),
+                on_change=lambda value: AppState.set_inputed_value(value).debounce(300),
                 width="100%",
             ),
             width="100%",
         ),
         rx.flex(
             challegeFilter(
+                classNamePrefix="filter",
                 options=FUND_SELECT_OPTIONS,
                 placeholder="ファンド",
                 onChange=lambda value: AppState.set_selected_fund_value(value),
                 isMulti=True,
-                width=["100%", "100%", "48%", "24%", "24%"],
+                styles=None,
+                theme=None,
+                width=["100%", "100%", "49.5%", "49.5%", "49.5%"],
             ),
             challegeFilter(
-                options=CHALLENGE_OPTIONS,
+                options=AppState.challenge_options,
+                classNamePrefix="filter",
                 placeholder="チャレンジ",
                 onChange=lambda value: AppState.set_selected_chllenge_value(value),
                 isMulti=True,
-                width=["100%", "100%", "48%", "24%", "24%"],
-            ),
-            challegeFilter(
-                options=FUNDING_STATUS_OPTIONS,
-                placeholder="投票ステータス",
-                onChange=lambda value: AppState.set_selected_fundingStatus_value(value),
-                isMulti=True,
-                width=["100%", "100%", "48%", "24%", "24%"],
-            ),
-            challegeFilter(
-                options=PROJECT_STATUS_OPTIONS,
-                placeholder="プロジェクト進捗",
-                onChange=lambda value: AppState.set_selected_projectStatus_value(value),
-                isMulti=True,
-                width=["100%", "100%", "48%", "24%", "24%"],
+                styles=None,
+                theme=None,
+                width=["100%", "100%", "49.5%", "49.5%", "49.5%"],
             ),
             width="100%",
             spacing="2",
             justify="between",
-            display=["block", "block", "flex", "flex", "flex"],
+            direction={"base": "column", "md": "row"},
+            flex_wrap="wrap",
+            row_gap="10px",
+        ),
+        rx.flex(
+            challegeFilter(
+                options=FUNDING_STATUS_OPTIONS,
+                classNamePrefix="filter",
+                placeholder="資金状況",
+                onChange=lambda value: AppState.set_selected_fundingStatus_value(value),
+                isMulti=True,
+                styles=None,
+                theme=None,
+                width=["100%", "100%", "49.5%", "49.5%", "49.5%"],
+            ),
+            challegeFilter(
+                options=PROJECT_STATUS_OPTIONS,
+                classNamePrefix="filter",
+                placeholder="プロジェクト状況",
+                onChange=lambda value: AppState.set_selected_projectStatus_value(value),
+                isMulti=True,
+                styles=None,
+                theme=None,
+                width=["100%", "100%", "49.5%", "49.5%", "49.5%"],
+            ),
+            width="100%",
+            spacing="2",
+            justify="between",
+            direction={"base": "column", "md": "row"},
             flex_wrap="wrap",
             row_gap="10px",
         ),
@@ -120,31 +151,31 @@ def catalyst() -> rx.Component:
 
     header = rx.flex(
         rx.flex(
-            rx.text(AppState.total_items, size="6", weight="bold", trim="end", color_scheme="crimson"),
-            rx.text("件"),
-            spacing="2",
+            rx.text(AppState.total_items, size="6", weight="bold", color="var(--indigo-11)"),
+            rx.text("件", size="4"),
             align_items="baseline",
             margin_left="5px",
-        ),
-        rx.desktop_only(
-            rx.hstack(
-            rx.button(
-                rx.icon("list"),
-                variant=rx.cond(AppState.view_mode == "list", "solid", "soft"),
-                color_scheme="indigo",
-                size="2",
-                on_click=lambda: AppState.set_view_mode("list"),
-            ),
-            rx.button(
-                rx.icon("layout-grid"),
-                variant=rx.cond(AppState.view_mode == "grid", "solid", "soft"),
-                color_scheme="indigo",
-                size="2",
-                on_click=lambda: AppState.set_view_mode("grid"),
-            ),
             spacing="2",
-            align_items="center",
-            ),
+        ),
+        rx.tablet_and_desktop(
+            rx.hstack(
+                rx.button(
+                    rx.icon("list"),
+                    variant=rx.cond(AppState.view_mode == "list", "solid", "soft"),
+                    color_scheme="indigo",
+                    size="2",
+                    on_click=lambda: AppState.set_view_mode("list"),
+                ),
+                rx.button(
+                    rx.icon("layout-grid"),
+                    variant=rx.cond(AppState.view_mode == "grid", "solid", "soft"),
+                    color_scheme="indigo",
+                    size="2",
+                    on_click=lambda: AppState.set_view_mode("grid"),
+                ),
+                spacing="2",
+                align_items="center",
+            )
         ),
         width="100%",
         justify_content="space-between",
@@ -166,17 +197,13 @@ def catalyst() -> rx.Component:
             ),
             width="100%",
             max_width="1130px",
-            #margin_x="auto",
         ),
-        rx.box(
-            rx.spinner(size="3"),
-            padding_y="15px",
-        ),
+        rx.box(rx.spinner(size="3"), padding_y="15px"),
     )
 
 
-@template(route="/catalyst/[proposal_id]", title="カタリストファンド | 詳細", on_load=ProposalAppState.on_load)
-def proposal_detail_page() -> rx.Component:
+@template(route="/catalyst/[proposal_id]", title="カタリスト | 詳細", on_load=ProposalAppState.on_load)
+def proposal_detail_page():
     return rx.vstack(
         detail_foreach_dict(),
         margin_top="15px",
